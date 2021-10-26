5347f58a1b728956eb129f9d8dccfddadc67f8c1
Niti Modi <nitimodi@microsoft.com>
2021-10-26 15:52:10 +0530

    added calculator functionality

---
+++ SampleFunctionApp/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleSumHttpTrigger
+{
+    public class Class1
+    {
+        public string num1 { get; set; }
+        public string num2 { get; set; }
+        public string operation { get; set; }
+
+
+    }
+}
